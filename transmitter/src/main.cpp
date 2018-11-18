cea39e2aed976f772dad6ef3b40d9ad4d0c77e26
greg <gmueth@gmail.com>
2018-11-18 15:40:44 -0500

    WIP: Button triggers interrupt. Main loop doesn't execute.

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -1,27 +1,55 @@
+#include <Arduino.h>
 #include <ELECHOUSE_CC1101.h>
 
-const int buttonPin1 = 3;    // the pin that the pushbutton is attached to
-const int buttonPin2 = 4;
-int i = 0;
+const byte ledPin = 13;
+const byte interruptPin = 3;
+volatile byte state = LOW;
+volatile int i = 0;
+volatile int flag = 0;
+
+void buttonISR();
+void transmit();
 
 void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
+
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonISR, FALLING);
 
   Serial.begin(9600);
   ELECHOUSE_cc1101.Init(F_433);
+
 }
 
 void loop() {
-  
-  if (!buttonPin1) {
-    i++;
-    String tx_message = "testing: " + String(i);
-    int m_length = tx_message.length();
-    byte txbyte[m_length];
-    tx_message.getBytes(txbyte, m_length + 1);
-    Serial.println((char *)txbyte);
-    ELECHOUSE_cc1101.SendData(txbyte, m_length);
+
+  Serial.println(flag);
+  if(flag == 1) {
+    Serial.println("Transmitting...");
+    transmit();
+    flag = 0;
   }
-  delay(100);
+  delay(500);
+
+  digitalWrite(ledPin, state);
+}
+
+void buttonISR() {
+  state = !state;
+  flag = 1;
+  // Must remove Serial.print too slow for interrupt
+  Serial.println("Interupt Triggered.");
+  return;
+}
+
+void transmit() {
+  i++;
+  String tx_message = "testing: " + String(i);
+  int m_length = tx_message.length();
+  byte txbyte[m_length];
+  tx_message.getBytes(txbyte, m_length + 1);
+  Serial.println((char *)txbyte);
+  ELECHOUSE_cc1101.SendData(txbyte, m_length);
+  return;
 }
